eb34596e274466f253502263282099caeebefc5f
jihoo2002 <yolohoo@naver.com>
2024-11-20 09:30:53 +0900

    메세지 six

--- src/p202111075/Main.java
+++ src/p202111075/Main.java
@@ -1,17 +1,15 @@
 package p202111075;
-
+import static java.lang.System.out;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("202111075 권지후");
-		System.out.println(Hello.sum(10));
-System.out.println(Hello.sum(100));
-System.out.println(Hello.sum(1000));
-System.out.println(World.factorial(3));
-System.out.println(World.factorial(6));
-System.out.println(World.factorial(9));
-System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
-System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+		out.println("201514099 홍길동");
+		out.println(Hello.sum(10));
+		out.println(Hello.sum(100));
+		out.println(Hello.sum(1000));
+		out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+		out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+
 
 
 	}
