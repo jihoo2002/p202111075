679f3b6980bc32becdf756395013a5543e384a31
jihoo2002 <yolohoo@naver.com>
2024-11-20 09:26:39 +0900

    메세지 five

--- src/p202111075/Main.java
+++ src/p202111075/Main.java
@@ -10,6 +10,9 @@ System.out.println(Hello.sum(1000));
 System.out.println(World.factorial(3));
 System.out.println(World.factorial(6));
 System.out.println(World.factorial(9));
+System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+
 
 	}
 }
